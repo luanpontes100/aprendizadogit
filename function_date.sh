577b43d55487373db3926bee150ed1315276103c
luanpontes100 <luanpontes100@hotmail.com>
2021-05-31 14:20:36 -0300

    data

--- function_date.sh
+++ function_date.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-VAR1=$1
+cut_date () {
+		if [[ $1 =~ "/" ]]; then
+			VALOR1=$(echo $1 | cut -d\/ -f1)
+                        VALOR2=$(echo $1 | cut -d\/ -f2)
+                        VALOR3=$(echo $1 | cut -d\/ -f3)
+		else
+			VALOR1=$(echo $1 | sed -r 's/^[0-9][0-9]/&\//' | sed -r 's/^[0-9][0-9]\/[0-9][0-9]/&\//' | cut -d\/ -f1)
+			VALOR2=$(echo $1 | sed -r 's/^[0-9][0-9]/&\//' | sed -r 's/^[0-9][0-9]\/[0-9][0-9]/&\//' | cut -d\/ -f2)
+			VALOR3=$(echo $1 | sed -r 's/^[0-9][0-9]/&\//' | sed -r 's/^[0-9][0-9]\/[0-9][0-9]/&\//' | cut -d\/ -f3)
+		fi
+		echo "${VALOR1}/${VALOR2}/${VALOR3}"
+}
 
+function_f () {
+}
