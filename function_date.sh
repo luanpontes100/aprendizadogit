321413310359dde20abf18e0b464bc7a3ad6e888
luanpontes100 <luanpontes100@hotmail.com>
2021-05-31 17:25:39 -0300

    Date function 1

--- function_date.sh
+++ function_date.sh
@@ -10,8 +10,26 @@ cut_date () {
 			VALOR2=$(echo $1 | sed -r 's/^[0-9][0-9]/&\//' | sed -r 's/^[0-9][0-9]\/[0-9][0-9]/&\//' | cut -d\/ -f2)
 			VALOR3=$(echo $1 | sed -r 's/^[0-9][0-9]/&\//' | sed -r 's/^[0-9][0-9]\/[0-9][0-9]/&\//' | cut -d\/ -f3)
 		fi
-		echo "${VALOR1}/${VALOR2}/${VALOR3}"
 }
 
 function_f () {
+	
+	cut_date $1
+	#if [ $VALOR1 -lt $VALOR2 -a $VALOR1 -le 31 -a $VALOR2 -le 12 ]; then
+#		echo 0
+#	elif [ $VALOR2 -lt $VALOR1 -a $VALOR2 -le 31 -a $VALOR1 -le 12 ]; #then
+#		echo 1
+#	elif [ $VALOR1 -eq $VALOR2 ]; then
+#		echo 2
+#	else
+#		echo 3
+#	fi
+	if [ $VALOR1 -le 31 ]; then
+		
+
+	fi
+		
+
 }
+
+function_f $1
