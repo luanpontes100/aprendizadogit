10cbae731690c5d3fd10b70c1f0bac7becf1e0bf
luanpontes100 <luanpontes100@hotmail.com>
2021-05-31 17:54:06 -0300

    tudo

--- function_date.sh
+++ function_date.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
 cut_date () {
-		if [[ $1 =~ "/" ]]; then
-			VALOR1=$(echo $1 | cut -d\/ -f1)
-                        VALOR2=$(echo $1 | cut -d\/ -f2)
-                        VALOR3=$(echo $1 | cut -d\/ -f3)
-		else
-			VALOR1=$(echo $1 | sed -r 's/^[0-9][0-9]/&\//' | sed -r 's/^[0-9][0-9]\/[0-9][0-9]/&\//' | cut -d\/ -f1)
-			VALOR2=$(echo $1 | sed -r 's/^[0-9][0-9]/&\//' | sed -r 's/^[0-9][0-9]\/[0-9][0-9]/&\//' | cut -d\/ -f2)
-			VALOR3=$(echo $1 | sed -r 's/^[0-9][0-9]/&\//' | sed -r 's/^[0-9][0-9]\/[0-9][0-9]/&\//' | cut -d\/ -f3)
-		fi
+	
+	if [[ $1 =~ "/" ]]; then
+		VALOR1=$(echo $1 | cut -d\/ -f1)
+                VALOR2=$(echo $1 | cut -d\/ -f2)
+                VALOR3=$(echo $1 | cut -d\/ -f3)
+	else
+		VALOR1=$(echo $1 | sed -r 's/^[0-9][0-9]/&\//' | sed -r 's/^[0-9][0-9]\/[0-9][0-9]/&\//' | cut -d\/ -f1)
+		VALOR2=$(echo $1 | sed -r 's/^[0-9][0-9]/&\//' | sed -r 's/^[0-9][0-9]\/[0-9][0-9]/&\//' | cut -d\/ -f2)
+		VALOR3=$(echo $1 | sed -r 's/^[0-9][0-9]/&\//' | sed -r 's/^[0-9][0-9]\/[0-9][0-9]/&\//' | cut -d\/ -f3)
+	fi
+
 }
 
 function_f () {
 	
 	cut_date $1
-	#if [ $VALOR1 -lt $VALOR2 -a $VALOR1 -le 31 -a $VALOR2 -le 12 ]; then
-#		echo 0
-#	elif [ $VALOR2 -lt $VALOR1 -a $VALOR2 -le 31 -a $VALOR1 -le 12 ]; #then
-#		echo 1
-#	elif [ $VALOR1 -eq $VALOR2 ]; then
-#		echo 2
-#	else
-#		echo 3
-#	fi
-	if [ $VALOR1 -le 31 ]; then
-		
-
-	fi
-		
+	if [ $VALOR1 -le 12 -a $VALOR2 -le 12 ]; then
+		echo 2
+	elif [ $VALOR1 -le 31 -a $VALOR2 -le 12 ]; then
+		echo 0
+	elif [ $VALOR2 -le 31 -a $VALOR1 -le 12 ]; then
+		echo 1
+	else
+		echo 3
+	fi	
 
 }
 
-function_f $1
+function_i () {
+
+	if 
+
+}
