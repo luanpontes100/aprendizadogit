b9e6cca5e3bc9ecae3c5c3719b43ae43e3c30fb0
luanpontes100 <luanpontes100@hotmail.com>
2021-06-01 13:52:36 -0300

    tudo

--- function_date.sh
+++ function_date.sh
@@ -31,6 +31,27 @@ function_f () {
 
 function_i () {
 
-	if 
+	 cut_date $1
+	if [ $VALOR1 -eq $VALOR2 -a $VALOR1 -le 12 -a $VALOR2 -le 12 ]; then
+		echo "${VALOR1}/${VALOR2}/${VALOR3}"
+ 	elif [ $VALOR1 -le 12 -a $VALOR2 -le 12 ]; then
+		echo "Impossivel determinar data"
+	elif [ $VALOR1 -le 31 -a $VALOR2 -le 12 ]; then
+		echo "${VALOR2}/${VALOR1}/${VALOR3}"
+	elif [ $VALOR2 -le 31 -a $VALOR1 -le 12 ]; then
+		echo "${VALOR1}/${VALOR2}/${VALOR3}"
+	else
+		echo "Data Inválida"
+	fi	
 
 }
+
+function_b (){
+
+	cut_date $1
+	echo "${VALOR1}/${VALOR2}/${VALOR3}"
+
+}
+function_f $1
+function_i $1
+function_b $1
