b1df0eb055feb562d4ae3312c1d1b12d41939c5c
luanpontes100 <luanpontes100@hotmail.com>
2021-07-26 17:09:24 -0300

    apagar backup

--- apagar_backup.sh
+++ apagar_backup.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 ################## LIMPAR BACKUP ###############
 #### SCRIPT PARA LIMPAR OS BACKUPS ANTIGOS E DEIXAR SOMENTE UM POR MÊS ##########
+############ FEITO PARA FREEBSD, PODE PRECISAR DE ADAPTAÇÕES PARA LINUX ########
+EXCLUSAO=()
+POSICAO=0
+TEMPO=0
 cd /mnt/iqnus
-for i in $(ls); do
-	cd $i;
-	find /home/backups -mtime +2 -exec rm -r {} \;
-
+for FOLDER in $(ls | egrep "gitlab|zabbix"); do
+	cd $FOLDER;
+	for DATE in $(stat -t "%m/%Y" -f "%N/%Sm" * | cut -d \/ -f 2,3 | sort -h | uniq); do
+			if [ $(find . -exec stat -t "%m/%Y" -f "%N/%Sm" {} \; | grep "$DATE" | wc -l | sed 's/  //g') = 1 ]; then
+				echo "Data $DATE já concluída"
+				continue
+			else
+				for FILE in $(find . -exec stat -t "%s/%m/%Y" -f "%N/%Sm" {} \; | grep "$DATE" ); do
+					if [ $(echo $FILE | cut -d\/ -f 3) -gt $TEMPO ]; then
+						TEMPO=$(echo $FILE | cut -d\/ -f 3) 
+						ARQUIVO=$(echo $FILE | cut -d\/ -f 2)
+						
+					else
+						EXCLUSAO[$POSICAO]=$(echo $FILE | cut -d\/ -f 2)
+						POSICAO=$(expr $POSICAO + 1)
+					fi
+				done
+			
+	done
