91595b74bc9d53dc75db0d95c7ea4fde5cc80d7a
luanpontes100 <luanpontes100@hotmail.com>
2021-07-26 17:56:52 -0300

    correção posição do array

--- apagar_backup.sh
+++ apagar_backup.sh
@@ -6,7 +6,6 @@ EXCLUSAO=()
 POSICAO=0
 TEMPO=0
 cd /mnt/iqnus
-<<<<<<< HEAD
 for FOLDER in $(ls | egrep "gitlab|zabbix"); do
 	cd $FOLDER;
 	for DATE in $(stat -t "%m/%Y" -f "%N/%Sm" * | cut -d \/ -f 2,3 | sort -h | uniq); do
@@ -16,20 +15,14 @@ for FOLDER in $(ls | egrep "gitlab|zabbix"); do
 			else
 				for FILE in $(find . -exec stat -t "%s/%m/%Y" -f "%N/%Sm" {} \; | grep "$DATE" ); do
 					if [ $(echo $FILE | cut -d\/ -f 3) -gt $TEMPO ]; then
+						if [-n $ARQUIVO ];then
+							EXCLUSAO[$POSICAO]=$(echo $ARQUIVO | cut -d\/ -f 2)
+						fi
 						TEMPO=$(echo $FILE | cut -d\/ -f 3) 
 						ARQUIVO=$(echo $FILE | cut -d\/ -f 2)
-						
 					else
 						EXCLUSAO[$POSICAO]=$(echo $FILE | cut -d\/ -f 2)
-						POSICAO=$(expr $POSICAO + 1)
 					fi
+					POSICAO=$(expr $POSICAO + 1)
 				done
-			
 	done
-=======
-for FOLDER in $(ls); do
-	cd $FOLDER
-	find . -newermt "2021-06-01" ! -newermt "2021-07-01" -exec stat -t "%s" -f "%N/%Sm" {} \;
-	
-
->>>>>>> c27fd808093d8a024c85a5f97a9089a5a1e2ff28
